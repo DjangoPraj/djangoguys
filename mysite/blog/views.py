8802556324b8c21b7feba652d14a77d5c4a68d55
DjangoPraj <prajwalvittalarao@gmail.com>
2019-02-28 19:55:34 +0530

    Added the views functions to fetch data from database

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    post_list=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    context={'post_list':post_list}
+    return render(request,'blog/post_list.html',context)
